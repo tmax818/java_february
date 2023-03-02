b8c5c13d2584473e4da16539d3471fff7215fd5b
tmax818 <tylermaxwell661@gmail.com>
2023-03-02 10:41:34 -0800

    update

--- algos/day01/Basic13.java
+++ algos/day01/Basic13.java
@@ -1,15 +1,62 @@
 package algos.day01;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Basic13 {
 
 // TODO: Print all the integers from 1 to 255.
 
+public static void Basic1(){
+
+    for(int i = 1; i <= 255; i++){
+        System.out.println(i);
+    }
+}
+
 // TODO: Print integers from 0 to 255, and with each integer print the sum so far.
+
+public static void Basic2(){
+    int sum = 0;
+    for(int i = 1; i <= 255; i++){
+        sum += i;
+        System.out.println(i);
+        System.out.println(sum);
+    }
+}
 
 // TODO: Given an array, find and print its largest element.
 
+public static void Basic3(int[] array){
+        int max = array[0];
+    for(int i = 0; i < array.length; i++){
+        if(array[i] > max){
+            max = array[i];
+        }
+    }
+    System.out.println(max);
+}
+
 // TODO: Create an array with all the odd integers between 1 and 255 (inclusive).
+
+public static void Basic4(){
+    int[] oddArray = new int[256];
+    for(int i = 0; i <= 255; i++){
+
+    }
+    System.out.println(Arrays.toString(oddArray));
+}
+
+public static void Basic4_2(){
+    ArrayList<Integer> oddArray = new ArrayList<>();
+    // System.out.println(Arrays.toString(oddArray));
+    for(int i = 0; i <= 255; i++){
+        if(i % 2 == 1){
+            oddArray.add(i);
+        }
+    }
+    System.out.println(oddArray);
+}
 
 // TODO: Given an array and a value Y, count and print the number of array values greater than Y.
 
---
+++ algos/day01/Main.java
@@ -0,0 +1,14 @@
+
+import algos.day01.Basic13;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Basic13.Basic1();
+        Basic13.Basic2();
+        int[] arr = {1,2,99,3,4};
+        Basic13.Basic3(arr);
+        Basic13.Basic4();
+        Basic13.Basic4_2();
+    }
+}
